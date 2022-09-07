c1a6706eccba19d085b575d1cd2b866dbcc07463
Enver Yilmaz <109732583+Envyilmaz@users.noreply.github.com>
2022-09-07 23:11:49 +0300

    uploaded.c

---
+++ c03/ex03/ft_strncat.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: envyilma <envyilma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/07 14:09:42 by envyilma          #+#    #+#             */
+/*   Updated: 2022/09/07 14:09:44 by envyilma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (j < nb && src[j] != '\0')
+	{
+		dest[i + j] = src [j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
